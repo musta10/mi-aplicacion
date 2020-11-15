151877aa7f6cd3e91ffdac9361c4600973ea6a99
musta <mustafaenfadal@gmail.com>
2020-11-15 20:03:41 +0100

    primer commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         esta es mi aplicacion usando las mismas tecnologias que usan en Facebook 
         </p>
         <a
           className="App-link"
